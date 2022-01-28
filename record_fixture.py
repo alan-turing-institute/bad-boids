0522ab4d82cc41a58979d58e5e8306cd25153ca2
jack89roberts <jroberts@turing.ac.uk>
2022-01-28 12:27:21 +0000

    Add regression test

---
+++ record_fixture.py
@@ -0,0 +1,10 @@
+from copy import deepcopy
+import yaml
+import boids
+
+before = deepcopy(boids.boids)
+boids.update_boids(boids.boids)
+after = boids.boids
+fixture = {"before": before, "after": after}
+with open("fixture.yml", "w") as fixture_file:
+    fixture_file.write(yaml.safe_dump(fixture))
