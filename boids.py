5da3f589a39bfa195389fff878818d9d42e5a58b
jack89roberts <jroberts@turing.ac.uk>
2022-01-28 16:34:53 +0000

    Refactor Boids structure into a class

--- boids.py
+++ boids.py
@@ -11,94 +11,96 @@ import random
 # Deliberately terrible code for teaching purposes
 
 
-def get_flock_parameters(boid_count):
-    return {
-        "flock_attraction": 0.01 / boid_count,
-        "avoidance_radius": 10,
-        "formation_flying_radius": 100,
-        "speed_matching_strength": 0.125 / boid_count,
-    }
+class Boids:
+    def __init__(self, parameters):
+        self.parameters = parameters
 
+    @classmethod
+    def with_default_parameters(cls, boid_count):
+        parameters = {
+            "flock_attraction": 0.01 / boid_count,
+            "avoidance_radius": 10,
+            "formation_flying_radius": 100,
+            "speed_matching_strength": 0.125 / boid_count,
+        }
+        return cls(parameters)
 
-def initialise_boids(
-    boid_count,
-    x_range=(-450, 50.0),
-    y_range=(300.0, 600.0),
-    xv_range=(0, 10.0),
-    yv_range=(-20.0, 20.0),
-):
-    boids_x = [random.uniform(*x_range) for _ in range(boid_count)]
-    boids_y = [random.uniform(*y_range) for _ in range(boid_count)]
-    boid_x_velocities = [random.uniform(*xv_range) for _ in range(boid_count)]
-    boid_y_velocities = [random.uniform(*yv_range) for _ in range(boid_count)]
-    return (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
+    def initialise_random(
+        self,
+        boid_count,
+        x_range=(-450, 50.0),
+        y_range=(300.0, 600.0),
+        xv_range=(0, 10.0),
+        yv_range=(-20.0, 20.0),
+    ):
+        self.xs = [random.uniform(*x_range) for _ in range(boid_count)]
+        self.ys = [random.uniform(*y_range) for _ in range(boid_count)]
+        self.xvs = [random.uniform(*xv_range) for _ in range(boid_count)]
+        self.yvs = [random.uniform(*yv_range) for _ in range(boid_count)]
 
+    def initialise_from_data(self, data):
+        self.xs, self.ys, self.xvs, self.yvs = data
 
-def boid_interaction(me, other, flock_parameters):
-    my_x, my_y, my_xv, my_yv = me
-    their_x, their_y, their_xv, their_yv = other
+    def boid_interaction(self, me, other):
+        my_x, my_y, my_xv, my_yv = me
+        their_x, their_y, their_xv, their_yv = other
 
-    x_separation = their_x - my_x
-    y_separation = their_y - my_y
+        x_separation = their_x - my_x
+        y_separation = their_y - my_y
 
-    delta_xv = 0
-    delta_yv = 0
-    # Fly towards the middle
-    delta_xv += x_separation * flock_parameters["flock_attraction"]
-    delta_yv += y_separation * flock_parameters["flock_attraction"]
-    # Fly away from nearby boids
-    separation_sq = x_separation ** 2 + y_separation ** 2
-    if separation_sq < flock_parameters["avoidance_radius"] ** 2:
-        delta_xv -= x_separation
-        delta_yv -= y_separation
-    # Try to match speed with nearby boids
-    if separation_sq < flock_parameters["formation_flying_radius"] ** 2:
-        delta_xv += (their_xv - my_xv) * flock_parameters["speed_matching_strength"]
-        delta_yv += (their_yv - my_yv) * flock_parameters["speed_matching_strength"]
+        delta_xv = 0
+        delta_yv = 0
+        # Fly towards the middle
+        delta_xv += x_separation * self.parameters["flock_attraction"]
+        delta_yv += y_separation * self.parameters["flock_attraction"]
+        # Fly away from nearby boids
+        separation_sq = x_separation ** 2 + y_separation ** 2
+        if separation_sq < self.parameters["avoidance_radius"] ** 2:
+            delta_xv -= x_separation
+            delta_yv -= y_separation
+        # Try to match speed with nearby boids
+        if separation_sq < self.parameters["formation_flying_radius"] ** 2:
+            delta_xv += (their_xv - my_xv) * self.parameters["speed_matching_strength"]
+            delta_yv += (their_yv - my_yv) * self.parameters["speed_matching_strength"]
 
-    return delta_xv, delta_yv
+        return delta_xv, delta_yv
 
+    def update(self):
+        # Compute boid velocity updates
+        delta_xvs = [0] * len(self.xs)
+        delta_yvs = [0] * len(self.xs)
+        for i in range(len(self.xs)):
+            me = (self.xs[i], self.ys[i], self.xvs[i], self.yvs[i])
+            for j in range(len(self.xs)):
+                other = (self.xs[j], self.ys[j], self.xvs[j], self.yvs[j])
+                dxv, dyv = self.boid_interaction(me, other)
+                delta_xvs[i] += dxv
+                delta_yvs[i] += dyv
 
-def update_boids(boids, flock_parameters):
-    xs, ys, xvs, yvs = boids
-
-    # Compute boid velocity updates
-    delta_xvs = [0] * len(xs)
-    delta_yvs = [0] * len(xs)
-    for i in range(len(xs)):
-        me = (xs[i], ys[i], xvs[i], yvs[i])
-        for j in range(len(xs)):
-            other = (xs[j], ys[j], xvs[j], yvs[j])
-            dxv, dyv = boid_interaction(me, other, flock_parameters)
-            delta_xvs[i] += dxv
-            delta_yvs[i] += dyv
-
-    # Apply updates
-    for i in range(len(xs)):
-        # Update velocities
-        xvs[i] += delta_xvs[i]
-        yvs[i] += delta_yvs[i]
-        # Move according to velocities
-        xs[i] += xvs[i]
-        ys[i] += yvs[i]
+        # Apply updates
+        for i in range(len(self.xs)):
+            # Update velocities
+            self.xvs[i] += delta_xvs[i]
+            self.yvs[i] += delta_yvs[i]
+            # Move according to velocities
+            self.xs[i] += self.xvs[i]
+            self.ys[i] += self.yvs[i]
 
 
 boid_count = 50
-flock_parameters = get_flock_parameters(boid_count)
-boids = initialise_boids(boid_count)
+boids = Boids.with_default_parameters(boid_count)
+boids.initialise_random(boid_count)
 figure = plt.figure()
 axes = plt.axes(xlim=(-500, 1500), ylim=(-500, 1500))
-scatter = axes.scatter(boids[0], boids[1])
+scatter = axes.scatter(boids.xs, boids.ys)
 
 
-def animate(frame, flock_parameters):
-    update_boids(boids, flock_parameters)
+def animate(frame):
+    boids.update()
     scatter.set_offsets(list(zip(boids[0], boids[1])))
 
 
-anim = animation.FuncAnimation(
-    figure, animate, frames=50, interval=50, fargs=(flock_parameters,)
-)
+anim = animation.FuncAnimation(figure, animate, frames=50, interval=50)
 
 if __name__ == "__main__":
     plt.show()
--- test_boids.py
+++ test_boids.py
@@ -1,48 +1,46 @@
 import os
 import yaml
 from pytest import approx
-from boids import get_flock_parameters, initialise_boids, update_boids
+from boids import Boids
 
 
 def test_bad_boids_regression():
     with open(os.path.join(os.path.dirname(__file__), "fixture.yml")) as fixture_file:
         regression_data = yaml.safe_load(fixture_file)
-
-    boid_data = regression_data["before"]
     boid_count = 50
-    flock_parameters = get_flock_parameters(boid_count)
-    update_boids(
-        boid_data,
-        flock_parameters,
-    )
-    for after, before in zip(regression_data["after"], boid_data):
+    boids = Boids.with_default_parameters(boid_count)
+    boids.initialise_from_data(regression_data["before"])
+    boids.update()
+    current_data = (boids.xs, boids.ys, boids.xvs, boids.yvs)
+    for after, before in zip(regression_data["after"], current_data):
         for after_value, before_value in zip(after, before):
             assert after_value == approx(before_value)
 
 
 def test_bad_boids_initialisation():
-    boid_count = 50
+    boid_count = 15
+    boids = Boids.with_default_parameters(boid_count)
     x_range = (-450, 50.0)
     y_range = (300.0, 600.0)
     xv_range = (0, 10.0)
     yv_range = (-20.0, 20.0)
-    xs, ys, xvs, yvs = initialise_boids(
+    boids.initialise_random(
         boid_count,
         x_range=x_range,
         y_range=y_range,
         xv_range=xv_range,
         yv_range=yv_range,
     )
-    assert len(xs) == boid_count
-    for x in xs:
+    assert len(boids.xs) == boid_count
+    for x in boids.xs:
         assert x < x_range[1]
         assert x > x_range[0]
-    for y in ys:
+    for y in boids.ys:
         assert y < y_range[1]
         assert y > y_range[0]
-    for xv in xvs:
+    for xv in boids.xvs:
         assert xv < xv_range[1]
         assert xv > xv_range[0]
-    for yv in yvs:
+    for yv in boids.yvs:
         assert yv < yv_range[1]
         assert yv > yv_range[0]
