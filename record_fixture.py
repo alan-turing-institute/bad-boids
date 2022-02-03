a0111218f4729b184232265f5958a49830ff689a
jack89roberts <jroberts@turing.ac.uk>
2022-02-03 17:38:44 +0000

    doc strings

--- record_fixture.py
+++ record_fixture.py
@@ -3,17 +3,39 @@ from boids import Flock
 
 
 def boids_to_coords(boids):
+    """Convert a list of Boid into lists of coordinates
+
+    Parameters
+    ----------
+    boids : list
+        List of Boid instances
+
+    Returns
+    -------
+    tuple
+        Length 4 tuple containing lists of x, y, xv and yv coordinates for each Boid
+    """
+    # Convert to standard Python floats below as yaml.dump raises RepresenterError
+    # with numpy floats.
     return (
-        [float(boid.position[0]) for boid in boids],
-        [float(boid.position[1]) for boid in boids],
-        [float(boid.velocity[0]) for boid in boids],
-        [float(boid.velocity[1]) for boid in boids],
+        [float(boid.position[0]) for boid in boids],  # x
+        [float(boid.position[1]) for boid in boids],  # y
+        [float(boid.velocity[0]) for boid in boids],  # xv
+        [float(boid.velocity[1]) for boid in boids],  # yv
     )
 
 
-def record_fixture():
-    boid_count = 50
-    random_seed = 123
+def record_fixture(boid_count=50, random_seed=123):
+    """Save a yaml of Boid coordinates before and after an update to use in a
+    regression test.
+
+    Parameters
+    ----------
+    boid_count : int, optional
+        Number of Boid, by default 50
+    random_seed : int, optional
+        Random seed for generating initial Boid coordinates, by default 123
+    """
     flock = Flock.with_default_parameters(boid_count)
     flock.initialise_random(boid_count, random_seed=random_seed)
     before = boids_to_coords(flock.boids)
