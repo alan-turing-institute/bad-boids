e289286ef996c9aae2547dd11a559d58400b74b7
jack89roberts <jroberts@turing.ac.uk>
2022-02-03 16:44:09 +0000

    update recording fixture and make it reproducible

--- record_fixture.py
+++ record_fixture.py
@@ -1,10 +1,28 @@
-from copy import deepcopy
 import yaml
-import boids
+from boids import Flock
 
-before = deepcopy(boids.boids)
-boids.updateBoids(boids.boids)
-after = boids.boids
-fixture = {"before": before, "after": after}
-with open("fixture.yml", "w") as fixture_file:
-    fixture_file.write(yaml.safe_dump(fixture))
+
+def boids_to_coords(boids):
+    return (
+        [float(boid.position[0]) for boid in boids],
+        [float(boid.position[1]) for boid in boids],
+        [float(boid.velocity[0]) for boid in boids],
+        [float(boid.velocity[1]) for boid in boids],
+    )
+
+
+def record_fixture():
+    boid_count = 50
+    random_seed = 123
+    flock = Flock.with_default_parameters(boid_count)
+    flock.initialise_random(boid_count, random_seed=random_seed)
+    before = boids_to_coords(flock.boids)
+    flock.update()
+    after = boids_to_coords(flock.boids)
+    fixture = {"before": before, "after": after}
+    with open("fixture.yml", "w") as fixture_file:
+        fixture_file.write(yaml.safe_dump(fixture))
+
+
+if __name__ == "__main__":
+    record_fixture()
