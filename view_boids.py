659650efde0f0bbd77ed973883f7124830799393
jack89roberts <jroberts@turing.ac.uk>
2022-02-03 17:38:56 +0000

    Refactor animation script

--- view_boids.py
+++ view_boids.py
@@ -5,22 +5,46 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 from boids import Flock
 
-boid_count = 50
-flock = Flock.with_default_parameters(boid_count)
-flock.initialise_random(boid_count)
-figure = plt.figure()
-axes = plt.axes(xlim=(-500, 1500), ylim=(-500, 1500))
-scatter = axes.scatter(
-    [b.position[0] for b in flock.boids], [b.position[1] for b in flock.boids]
-)
 
+def view_boids(flock, xlim=(-500, 1500), ylim=(-500, 1500), frames=50, interval=50):
+    """Display an animation of a Flock of Boid
 
-def animate(_):  # Needs an argument to be compatible with animation.FuncAnimation
-    flock.update()
-    scatter.set_offsets([b.position for b in flock.boids])
+    Parameters
+    ----------
+    flock : Flock
+        The Flock of Boid to animate
+    xlim : tuple, optional
+        x-axis limits for animation, by default (-500, 1500)
+    ylim : tuple, optional
+        y-axis limits for animation, by default (-500, 1500)
+    frames : int, optional
+        Number of Flock updates in the animation, by default 50
+    interval : int, optional
+        Time between animation frames in milliseconds, by default 50
+    """
+    figure = plt.figure()
+    axes = plt.axes(xlim=xlim, ylim=ylim)
+    scatter = axes.scatter(
+        [b.position[0] for b in flock.boids], [b.position[1] for b in flock.boids]
+    )
 
+    def animate(_):
+        """Update the flock and the Boid positions in the scatter plot
 
-anim = animation.FuncAnimation(figure, animate, frames=50, interval=50)
+        Parameters
+        ----------
+        _ : None
+             Needed for compatiblity with animation.FuncAnimation
+        """
+        flock.update()
+        scatter.set_offsets([b.position for b in flock.boids])
+
+    _ = animation.FuncAnimation(figure, animate, frames=frames, interval=interval)
+    plt.show()
+
 
 if __name__ == "__main__":
-    plt.show()
+    boid_count = 50
+    flock = Flock.with_default_parameters(boid_count)
+    flock.initialise_random(boid_count)
+    view_boids(flock)
