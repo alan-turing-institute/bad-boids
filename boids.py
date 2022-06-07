1d8ca0197361b27e297cb40046076de0ed8ad965
Andreas <35858164+a-doering@users.noreply.github.com>
2022-06-07 22:13:43 +0200

    Split code to animation.py

--- boids.py
+++ boids.py
@@ -3,18 +3,17 @@ A deliberately bad implementation of [Boids](http://dl.acm.org/citation.cfm?doid
 for use as an exercise on refactoring.
 """
 
-from matplotlib import pyplot as plt
-from matplotlib import animation
 import random
 import yaml
 
 # Deliberately terrible code for teaching purposes
-
-boids_x=[random.uniform(-450,50.0) for x in range(50)]
-boids_y=[random.uniform(300.0,600.0) for x in range(50)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]
-boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
-boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
+def initialize_boids():
+    boids_x=[random.uniform(-450,50.0) for x in range(50)]
+    boids_y=[random.uniform(300.0,600.0) for x in range(50)]
+    boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]
+    boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
+    boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
+    return boids
 
 def updateBoids(boids):
     xs,ys,xvs,yvs=boids
@@ -47,19 +46,3 @@ def updateBoids(boids):
     for i in range(len(xs)):
         xs[i]=xs[i]+xvs[i]
         ys[i]=ys[i]+yvs[i]
-
-
-figure=plt.figure()
-axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
-scatter=axes.scatter(boids[0],boids[1])
-
-def ANIMATE(frame):
-    updateBoids(boids)
-    scatter.set_offsets(list(zip(boids[0],boids[1])))
-
-
-anim = animation.FuncAnimation(figure, ANIMATE,
-                               frames=50, interval=50)
-
-if __name__ == "__main__":
-    plt.show()
