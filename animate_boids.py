8495b17e8c963dba3f52b177a028b09d4c0551dc
Andreas <35858164+a-doering@users.noreply.github.com>
2022-07-03 16:01:00 +0200

    Add flake8 and black linting

--- animate_boids.py
+++ animate_boids.py
@@ -1,18 +1,19 @@
 from matplotlib import pyplot as plt
 from matplotlib import animation
 from boids import updateBoids, initialize_boids
+
 boids = initialize_boids()
-figure=plt.figure()
-axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
-scatter=axes.scatter(boids[0],boids[1])
+figure = plt.figure()
+axes = plt.axes(xlim=(-500, 1500), ylim=(-500, 1500))
+scatter = axes.scatter(boids[0], boids[1])
+
 
 def ANIMATE(frame):
     updateBoids(boids)
-    scatter.set_offsets(list(zip(boids[0],boids[1])))
+    scatter.set_offsets(list(zip(boids[0], boids[1])))
 
 
-anim = animation.FuncAnimation(figure, ANIMATE,
-                               frames=50, interval=50)
+anim = animation.FuncAnimation(figure, ANIMATE, frames=50, interval=50)
 
 if __name__ == "__main__":
     plt.show()
--- boids.py
+++ boids.py
@@ -4,15 +4,17 @@ for use as an exercise on refactoring.
 """
 import numpy as np
 import random
-class Boid():
-    def __init__(self, x,y,vx,vy, owner):
-        self.position = np.array([x,y])
+
+
+class Boid:
+    def __init__(self, x, y, vx, vy, owner):
+        self.position = np.array([x, y])
         self.velocity = np.array([vx, vy])
         self.owner = owner
 
     def interact(self, other):
         """Compute velocity changes due to one other boid"""
-        
+
         delta_v = np.array([0.0, 0.0])
         separation = other.position - self.position
         separation_square = separation.dot(separation)
@@ -21,28 +23,30 @@ class Boid():
         delta_v += separation * self.owner.parameters["flock_attraction"]
 
         # Fly away from nearby boids
-        if separation_square < self.owener.parameters["avoidance_radius"]**2:
+        if separation_square < self.owener.parameters["avoidance_radius"] ** 2:
             delta_v -= separation
-        
+
         # Try to match speed with nearby boids
-        if separation_square < self.owner.parameters["formation_flying_radius"]**2:
-            delta_v += (other.velocity - self.velocity) * self.owner.parameters["speed_matching_strength"]
+        if separation_square < self.owner.parameters["formation_flying_radius"] ** 2:
+            delta_v += (other.velocity - self.velocity) * self.owner.parameters[
+                "speed_matching_strength"
+            ]
         return delta_v
 
     def move(self, delta_v):
         """Update the position and velocity of the Boid"""
         self.velocity += delta_v
         self.position += self.velocity
-    
 
-class Flock():
+
+class Flock:
     def __init__(self, parameters):
         self.parameters = parameters
         self.boids = []
 
     def __len__(self):
         return len(self.boids)
-    
+
     # Ownership of Boids
     # Adjust velocity: match speed, fly to center, fly away from others
     # Init flock
@@ -50,41 +54,42 @@ class Flock():
 
 # Deliberately terrible code for teaching purposes
 def initialize_boids():
-    boids_x=[random.uniform(-450,50.0) for x in range(50)]
-    boids_y=[random.uniform(300.0,600.0) for x in range(50)]
-    boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]
-    boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
-    boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
+    boids_x = [random.uniform(-450, 50.0) for x in range(50)]
+    boids_y = [random.uniform(300.0, 600.0) for x in range(50)]
+    boid_x_velocities = [random.uniform(0, 10.0) for x in range(50)]
+    boid_y_velocities = [random.uniform(-20.0, 20.0) for x in range(50)]
+    boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
     return boids
 
+
 def updateBoids(boids):
-    xs,ys,xvs,yvs=boids
-    deltaXVs=[0]*len(xs)
-    deltaYVs=[0]*len(xs)
+    xs, ys, xvs, yvs = boids
+    deltaXVs = [0] * len(xs)
+    deltaYVs = [0] * len(xs)
     # Fly towards the middle
     for i in range(len(xs)):
         for j in range(len(xs)):
-            deltaXVs[i]=deltaXVs[i]+(xs[j]-xs[i])*0.01/len(xs)
+            deltaXVs[i] = deltaXVs[i] + (xs[j] - xs[i]) * 0.01 / len(xs)
     for i in range(len(xs)):
         for j in range(len(xs)):
-            deltaYVs[i]=deltaYVs[i]+(ys[j]-ys[i])*0.01/len(xs)
+            deltaYVs[i] = deltaYVs[i] + (ys[j] - ys[i]) * 0.01 / len(xs)
     # Fly away from nearby boids
     for i in range(len(xs)):
         for j in range(len(xs)):
-            if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
-                deltaXVs[i]=deltaXVs[i]+(xs[i]-xs[j])
-                deltaYVs[i]=deltaYVs[i]+(ys[i]-ys[j])
+            if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < 100:
+                deltaXVs[i] = deltaXVs[i] + (xs[i] - xs[j])
+                deltaYVs[i] = deltaYVs[i] + (ys[i] - ys[j])
     # Try to match speed with nearby boids
     for i in range(len(xs)):
         for j in range(len(xs)):
-            if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
-                deltaXVs[i]=deltaXVs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
-                deltaYVs[i]=deltaYVs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
+            if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < 10000:
+                deltaXVs[i] = deltaXVs[i] + (xvs[j] - xvs[i]) * 0.125 / len(xs)
+                deltaYVs[i] = deltaYVs[i] + (yvs[j] - yvs[i]) * 0.125 / len(xs)
     # Update velocities
     for i in range(len(xs)):
-        xvs[i]=xvs[i]+deltaXVs[i]
-        yvs[i]=yvs[i]+deltaYVs[i]
+        xvs[i] = xvs[i] + deltaXVs[i]
+        yvs[i] = yvs[i] + deltaYVs[i]
     # Move according to velocities
     for i in range(len(xs)):
-        xs[i]=xs[i]+xvs[i]
-        ys[i]=ys[i]+yvs[i]
+        xs[i] = xs[i] + xvs[i]
+        ys[i] = ys[i] + yvs[i]
