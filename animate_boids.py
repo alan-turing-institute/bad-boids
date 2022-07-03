b371baf685d00c39f479c532db9e7177c0f5c52e
Andreas <35858164+a-doering@users.noreply.github.com>
2022-07-03 19:12:23 +0200

    Update animate boids

--- animate_boids.py
+++ animate_boids.py
@@ -1,19 +1,25 @@
 from matplotlib import pyplot as plt
 from matplotlib import animation
-from boids import updateBoids, initialize_boids
-
-boids = initialize_boids()
-figure = plt.figure()
-axes = plt.axes(xlim=(-500, 1500), ylim=(-500, 1500))
-scatter = axes.scatter(boids[0], boids[1])
+from boids import Boid, Flock
 
 
-def ANIMATE(frame):
-    updateBoids(boids)
-    scatter.set_offsets(list(zip(boids[0], boids[1])))
+def view_boids(flock, xlim=(-500, 1500), ylim=(-500, 1500), frames=50, interval=50):
+    figure = plt.figure()
+    axes = plt.axes(xlim=xlim, ylim=ylim)
+    scatter = axes.scatter(
+        [b.position[0] for b in flock.boids], [b.position[1] for b in flock.boids]
+    )
 
+    def animate(_):
+        flock.update()
+        scatter.set_offsets([b.position for b in flock.boids])
 
-anim = animation.FuncAnimation(figure, ANIMATE, frames=50, interval=50)
+    _ = animation.FuncAnimation(figure, animate, frames=frames, interval=interval)
+    plt.show()
+
 
 if __name__ == "__main__":
-    plt.show()
+    boid_count = 50
+    flock = Flock.with_default_parameters(boid_count)
+    flock.initialize_random(boid_count)
+    view_boids(flock)
