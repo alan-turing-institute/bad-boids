526ad6e1937289e3ac4e324f0583f10c1707d7ef
Andreas <35858164+a-doering@users.noreply.github.com>
2022-07-03 18:30:02 +0200

    Update tests to new boid naming

--- test_boids.py
+++ test_boids.py
@@ -1,44 +1,53 @@
 import os
 import yaml
 from nose.tools import assert_almost_equal, assert_equal, assert_greater, assert_less
-from boids import updateBoids, initialize_boids
+from boids import Flock
 
 
 def test_bad_boids_regression():
     with open(os.path.join(os.path.dirname(__file__), "fixture.yml")) as fixture_file:
         regression_data = yaml.safe_load(fixture_file)
-
-    boid_data = regression_data["before"]
-    updateBoids(boid_data)
-    for after, before in zip(regression_data["after"], boid_data):
-        for after_value, before_value in zip(after, before):
-            assert_almost_equal(after_value, before_value, delta=0.01)
+    boid_count = 50
+    flock = Flock.with_default_parameters(boid_count)
+    flock.initialize_from_data(regression_data["before"])
+    flock.update()
+    for index, boid in enumerate(flock.boids):
+        assert_almost_equal(
+            boid.position[0], regression_data["after"][0][index], delta=0.01
+        )
+        assert_almost_equal(
+            boid.position[1], regression_data["after"][1][index], delta=0.01
+        )
+        assert_almost_equal(
+            boid.velocity[0], regression_data["after"][2][index], delta=0.01
+        )
+        assert_almost_equal(
+            boid.velocity[1], regression_data["after"][3][index], delta=0.01
+        )
 
 
 def test_bad_boids_initialization():
-    boid_count = 50
+    boid_count = 15
+    flock = Flock.with_default_parameters(boid_count)
     x_range = (-450, 50.0)
     y_range = (300.0, 600.0)
     xv_range = (0, 10.0)
-    yv_range = (-20, 20.0)
-    xs, ys, xvs, yvs = initialize_boids(
-        boid_count=boid_count,
+    yv_range = (-20.0, 20.0)
+    flock.initialize_random(
+        boid_count,
         x_range=x_range,
         y_range=y_range,
         xv_range=xv_range,
         yv_range=yv_range,
     )
-
-    assert_equal(len(xs), boid_count)
-    for x in xs:
-        assert_less(x, x_range[1])
-        assert_greater(x, x_range[0])
-    for y in ys:
-        assert_less(y, y_range[1])
-        assert_greater(y, y_range[0])
-    for xv in xvs:
-        assert_less(xv, xv_range[1])
-        assert_greater(xv, xv_range[0])
-    for yv in yvs:
-        assert_less(yv, yv_range[1])
-        assert_greater(yv, yv_range[0])
+    assert_equal(len(flock.boids), boid_count)
+    assert_equal(flock.boid_count, boid_count)
+    for boid in flock.boids:
+        assert_less(boid.position[0], x_range[1])
+        assert_greater(boid.position[0], x_range[0])
+        assert_less(boid.position[1], y_range[1])
+        assert_greater(boid.position[1], y_range[0])
+        assert_less(boid.velocity[0], xv_range[1])
+        assert_greater(boid.velocity[0], xv_range[0])
+        assert_less(boid.velocity[1], yv_range[1])
+        assert_greater(boid.velocity[1], yv_range[0])
