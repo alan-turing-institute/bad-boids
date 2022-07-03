62daeed506caf392a8eff15cad5626789e00c19d
Andreas <35858164+a-doering@users.noreply.github.com>
2022-07-03 16:18:47 +0200

    Add test for initialization

--- test_boids.py
+++ test_boids.py
@@ -1,7 +1,7 @@
 import os
 import yaml
-from nose.tools import assert_almost_equal
-from boids import updateBoids
+from nose.tools import assert_almost_equal, assert_equal, assert_greater, assert_less
+from boids import updateBoids, initialize_boids
 
 
 def test_bad_boids_regression():
@@ -13,3 +13,20 @@ def test_bad_boids_regression():
     for after, before in zip(regression_data["after"], boid_data):
         for after_value, before_value in zip(after, before):
             assert_almost_equal(after_value, before_value, delta=0.01)
+
+
+def test_bad_boids_initialization():
+    xs, ys, xvs, yvs = initialize_boids()
+    assert_equal(len(xs), 50)
+    for x in xs:
+        assert_less(x, 50.0)
+        assert_greater(x, -450)
+    for y in ys:
+        assert_less(y, 600)
+        assert_greater(y, 300)
+    for xv in xvs:
+        assert_less(xv, 10.0)
+        assert_greater(xv, 0)
+    for yv in yvs:
+        assert_less(yv, 20.0)
+        assert_greater(yv, -20.0)
